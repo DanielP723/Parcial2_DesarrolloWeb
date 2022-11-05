66b72d99f5dea229a3d80bbd58b2315cfea0e91d
SantiagoVelasco <david.velasco.2020@upb.edu.co>
2022-11-05 11:11:00 -0500

    back recuperado

--- parcial2/backend/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const MongoDBC_1 = __importDefault(require("DB/MongoDBC"));
-const server = new MongoDBC_1.default();
-server.start();
--- parcial2/backend/dist/model/MongoModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MongoModel {
-    constructor() {
-    }
-    getProducts(page) {
-    }
-}
-exports.default = MongoModel;
